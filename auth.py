e220a3a06df2a0b8f91fceb3e565f842d740c4c4
compujosh <jresnico@terpmail.umd.edu>
2017-02-12 17:28:06 -0500

    auth setup (token must be copied manually). Display chat to console

---
+++ auth.py
@@ -0,0 +1,31 @@
+#This script is licensed under the Apace 2.0 License (http://www.apache.org/licenses/LICENSE-2.0)
+#This script is a derivative work of the script at https://developers.google.com/api-client-library/python/auth/installed-app
+
+import json
+import webbrowser
+import configparser
+import httplib2
+from oauth2client import client
+
+
+if __name__ == '__main__':
+	flow = client.flow_from_clientsecrets(
+		'client_secrets.json',
+		scope='https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.force-ssl',
+		redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+
+	auth_uri = flow.step1_get_authorize_url()
+	webbrowser.open(auth_uri)
+
+	auth_code = raw_input('Enter the auth code: ')
+
+	credentials = flow.step2_exchange(auth_code)
+	http_auth = credentials.authorize(httplib2.Http())
+  
+	outFile = open("authcode.txt","w")
+	outFile.write(auth_code)
+	outFile.close()
+	
+	outFile = open("token.txt","w")
+	outFile.write(str(credentials.get_access_token()))
+	outFile.close()
--- main.py
+++ main.py
@@ -1,20 +1,36 @@
-#!/usr/bin/python
-
+import sys
+import json
 import time
 import configparser
 import requests
 
+debug = 0
+
 config = configparser.ConfigParser()
 config.read('config.ini')
+nextPageToken = ''
+
+tokenFile = open("token.txt","r")
+token = tokenFile.read()
 
 while (True):
 	
-	url = 'https://content.googleapis.com/youtube/v3/liveChat/messages?liveChatId=EiEKGFVDZmhqblk4YVpkaldCMkc1NC1xR1BVZxIFL2xpdmU&part=snippet&key='
-	url += config['main']['yt_api']
+	liveChatID = 'EiEKGFVDZmhqblk4YVpkaldCMkc1NC1xR1BVZxIFL2xpdmU'
+	url = 'https://content.googleapis.com/youtube/v3/liveChat/messages?liveChatId='+liveChatID+'&part=snippet,authorDetails&pageToken='+nextPageToken
 	
-	headers = { 'referrer' : 'totallyrealwebsite.com' }
+	headers = { 'referer' : '', "Authorization": "Bearer "+config["OAuthAcc"]["token"] }
 	
 	r = requests.get(url, headers=headers)
 	
-	print str(r.json())
+	resp = r.json()
+	if (debug >= 2):
+		print json.dumps(resp, indent=4, sort_keys=True)
+	
+	nextPageToken = resp["nextPageToken"]
+	
+	msgs = resp["items"]
+	
+	for msg in msgs:
+		print '<'+msg["authorDetails"]["displayName"]+'> '+msg["snippet"]["displayMessage"]
+	
 	time.sleep(5)
