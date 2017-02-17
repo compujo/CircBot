d5623003d08825b1804d5a473e06be5a2011eccc
compujosh <jresnico@terpmail.umd.edu>
2017-02-17 13:58:45 -0500

    output credentials object as json when getting OAuth perm

--- auth.py
+++ auth.py
@@ -21,11 +21,15 @@ if __name__ == '__main__':
 
 	credentials = flow.step2_exchange(auth_code)
 	http_auth = credentials.authorize(httplib2.Http())
-  
+
+	outFile = open("OAuthCredentials.json","w")
+	outFile.write(str(credentials.to_json()))
+	outFile.close()
+
 	outFile = open("authcode.txt","w")
 	outFile.write(auth_code)
 	outFile.close()
-	
+
 	outFile = open("token.txt","w")
 	outFile.write(str(credentials.get_access_token()))
 	outFile.close()
